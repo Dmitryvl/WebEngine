ea4182f9e0d7e02326b1dca7e5b14ee92b7b8548
Dzmitry Prakapenka <dzmitry.prakapenka@outlook.com>
2016-03-28 22:24:15 +0300

    added core project

---
+++ WebEngine.Core/Entities/Role.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Role.cs" author="Dzmitry Prakapenka">
+//     All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace WebEngine.Core.Entities
+{
+	#region Usings
+	
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	
+	#endregion 
+
+	/// <summary>
+	/// <see cref="Role"/> class.
+	/// </summary>
+	public class Role
+	{
+		// Empty.
+	}
+}
